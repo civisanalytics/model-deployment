2e66aaf6a864706ec8a73928673e5ff00c0e2d83
Keith Myers-Crum <kcrum@civisanalytics.com>
2018-04-26 11:24:46 -0500

    Added changes to utils.py and and deployed_model.py necessary to deploy a muffnn model.

--- civisml_deploy/controllers/deployed_model.py
+++ civisml_deploy/controllers/deployed_model.py
@@ -1,12 +1,12 @@
 from urllib.parse import parse_qs
 
-from flask import Blueprint, current_app, jsonify, \
-    request, after_this_request, \
-    send_from_directory
+from flask import (Blueprint, current_app, jsonify, request,
+                   after_this_request, send_from_directory)
 import numpy as np
 import pandas as pd
 
-from civisml_deploy.utils import setup_logs, log_inputs_outputs
+from civisml_deploy.utils import (setup_logs, log_inputs_outputs,
+                                  make_input_matrix)
 
 
 general_logger, analytics_logger = setup_logs()
@@ -19,6 +19,11 @@ def index():
     """The app's index page. An index page (even if blank) is required for
     health checks on Platform. This index page dynamically generates form
     fields based on the features endpoing.
+
+    Returns
+    -------
+    str
+        This returns the model's name, per CivisML.
     """
     return send_from_directory('/static', 'index.html')
 
@@ -37,48 +42,53 @@ def predict():
     are taken in as query parameters, and a CSV file is sent in reponse.
     """
     general_logger.debug('predict called')
-    input_df = _query_string_to_df(request.query_string)
+    input_matrix, n_top_items, user = _query_string_to_inputs(
+        request.query_string, current_app.model_dict
+    )
 
-    general_logger.debug(input_df)
-    preds = np.atleast_2d(_predict(input_df, current_app.model_dict))
-
-    pred_dict = {}
-    # Pack predictions into dict
-    for i, col in enumerate(current_app.model_dict['model_target_columns']):
-        pred_dict[col] = preds[:, i].tolist()
-
+    # Returns dictionary: {'rating': [...], 'item': [...]}
+    pred_dict = _predict(input_matrix, n_top_items, current_app.model_dict)
     general_logger.debug('Writing preds to JSON: \n%s', pred_dict)
 
     @after_this_request
     def write_analytics(response):
-        log_inputs_outputs(input_df, preds, analytics_logger)
+        log_df = pd.DataFrame(pred_dict)
+        log_df['user'] = n_top_items*[user]
+        log_inputs_outputs(log_df, current_app.model_dict, analytics_logger)
+
         return response
 
     return jsonify(pred_dict)
 
 
-def _predict(df, model_dict):
-    """We break this function out separately to take advantage of memoization,
-    if that's something we'd like to add (see Flask-Cache docs for more).
-    """
-    df = df[model_dict['model_features']]
-    general_logger.debug('Input covariate df: %s', df)
+def _predict(input_matrix, n_top_items, model_dict):
+    preds = getattr(model_dict['model'], model_dict['method'])(input_matrix)
+    # This slice reverses the list and returns the first n_top_items from the
+    # reversed list.
+    inds = np.argsort(preds)[:-(n_top_items + 1):-1]
+    general_logger.debug("indices for top %s items: %s", n_top_items, inds)
 
-    preds = getattr(model_dict['model'], model_dict['method'])(df)
-
-    # Multioutput classifiers return lists, rather than np.arrays
-    if isinstance(preds, list):
-        preds = np.hstack(preds)
-
-    return preds
+    return {'rating': preds[inds].tolist(),
+            'item': model_dict['items'][inds].tolist()}
 
 
-def _query_string_to_df(query_string):
+def _query_string_to_inputs(query_string, model_dict):
     """Parse query string and plug it into a `pd.DataFrame`.
     """
+    general_logger.debug('query string: %s', query_string)
     qs_dict = parse_qs(query_string.decode('utf-8'))
     qs_dict.pop('civis_service_token', None)
-
     general_logger.debug('query string (token removed, if passed): %s',
                          qs_dict)
-    return pd.DataFrame(qs_dict, index=range(1))
+
+    if 'ntopitems' not in qs_dict:
+        general_logger.info("Returning only top item, as user didn't pass "
+                            "'ntopitems' to query string.")
+        n_top_items = 1
+    else:
+        n_top_items = int(qs_dict['ntopitems'][0])
+
+    input_matrix, user = make_input_matrix(qs_dict['user'][0], model_dict)
+
+    general_logger.debug("Shape of input_matrix: %s", input_matrix.shape)
+    return input_matrix, n_top_items, user
--- civisml_deploy/utils.py
+++ civisml_deploy/utils.py
@@ -1,45 +1,68 @@
+from copy import copy
 from datetime import datetime
-from io import StringIO
+from io import BytesIO, StringIO
 import logging
+import pickle
 import os
 
-from civis.ml import ModelPipeline
-import pandas as pd
+import civis
+import numpy as np
+from scipy import sparse
+from scipy.sparse import csr_matrix
 
 general_logger = logging.getLogger("civisml_deploy")
 CIVIS_SERVICE_VERSION = os.environ.get('CIVIS_SERVICE_VERSION')
+USER_PREFIX = os.environ.get('USER_PREFIX')
+ITEM_PREFIX = os.environ.get('ITEM_PREFIX')
 
 
-def _get_model_features(model_pipe):
-    """Get features from a `civis.ml.ModelPipeline`
+def _fetch_recommender(model_file_id, user_item_file_id):
+    """Pull down pickled muffnn recommendation engine and comma-separated list
+    of user-item columns in training data.
     """
-    model_features = []
-    target_cols = model_pipe.train_result_.metadata['data']['target_columns']
-    for feat in model_pipe.train_result_.metadata['data']['column_names_read']:
-        if feat not in target_cols:
-            model_features.append(feat)
+    # Load muffnn model from /files endpoint
+    modelbytes = BytesIO()
+    general_logger.debug('Fetching muffnn model with file_id %s...',
+                         model_file_id)
+    civis.io.civis_to_file(model_file_id, modelbytes)
 
-    return model_features
-
-
-def _fetch_civisml_model(job_id, run_id=None):
-    """Fetch a `civis.ml.ModelPipeline` given a job_id and optionally a run_id
-    """
-    if run_id is not None:
-        general_logger.debug('Fetching model with job_id %s and run_id %s...',
-                             job_id, run_id)
-        model_pipe = ModelPipeline.from_existing(train_job_id=job_id,
-                                                 train_run_id=run_id)
-    else:
-        general_logger.debug('Fetching latest run of model with job_id %s...',
-                             job_id)
-        model_pipe = ModelPipeline.from_existing(train_job_id=job_id)
-
-    model_features = _get_model_features(model_pipe)
-    model = model_pipe.estimator
+    modelbytes.seek(0)
+    model = pickle.load(modelbytes)
     general_logger.debug('model loaded.')
 
-    return model, model_pipe, model_features
+    # Get user-item matrix columns from original long data
+    colbytes = BytesIO()
+    civis.io.civis_to_file(user_item_file_id, colbytes)
+
+    colbytes.seek(0)
+    user_item_columns = colbytes.read().decode('utf-8').strip().split(',')
+
+    return model, user_item_columns
+
+
+def _get_users_items(user_item_columns):
+    """Given the list of user-item columns, this function returns the number of
+    users, the list of items, and a sparse diagonal matrix of size
+    (n_items, n_items).
+    """
+    items = []
+    n_users = 0
+    for col in user_item_columns:
+        # Ensure only users and items in column list
+        if col.startswith(USER_PREFIX):
+            n_users += 1
+        elif col.startswith(ITEM_PREFIX):
+            items.append(col)
+        else:
+            raise ValueError("Column '{}' doesn't start with either the user "
+                             "prefix '{}' or the item prefix '{}', as "
+                             "expected.".format(col, USER_PREFIX, ITEM_PREFIX))
+
+    n_items = len(items)
+    diag_item_matrix = sparse.diags([1], shape=(n_items, n_items),
+                                    format='csr')
+
+    return n_users, np.array(items), diag_item_matrix
 
 
 def setup_logs():
@@ -79,29 +102,25 @@ def setup_logs():
     return general_logger, analytics_logger
 
 
-def log_inputs_outputs(input_df, preds, analytics_logger):
+def log_inputs_outputs(log_df, model_dict, analytics_logger):
     """Write the input and output of a prediction call.
 
     Parameters
     ----------
-    input_df : pd.DataFrame
-        DataFrame holding the input data passed by the user.
-    preds : np.ndarray
-        The predictions output by your model.
+    log_df : pd.DataFrame
+        DataFrame holding the data to log.
     analytics_logger : logging.Logger
         Instance of analytics logger.
     """
-
-    # Write inputs, output predictions, and time stamp to logfile
-    logdf = pd.merge(input_df.reset_index(), pd.DataFrame(preds).reset_index(),
-                     on='index')
-    logdf['timestamp'] = datetime.utcnow().strftime('%Y_%m_%d_%H:%M:%S.%f')
+    log_df['timestamp'] = datetime.utcnow().strftime('%Y_%m_%d_%H:%M:%S.%f')
+    output_cols = copy(model_dict['analytic_logging_columns'])
 
     if CIVIS_SERVICE_VERSION:
-        logdf['civis_service_version'] = CIVIS_SERVICE_VERSION
+        log_df['civis_service_version'] = CIVIS_SERVICE_VERSION
+        output_cols.append('civis_service_version')
 
     outdf = StringIO()
-    logdf.drop('index', axis=1).to_csv(outdf, index=False, header=False)
+    log_df.to_csv(outdf, index=False, header=False)
     outdf.seek(0)
     analytics_logger.info(outdf.read().rstrip())
 
@@ -117,11 +136,7 @@ def output_analytics_log_columns(model_dict):
         `civis.ml.ModelPipeline` object and associated metadata.
     """
     # Output the column order of the analytics logs to stdout
-    output_cols = []
-    output_cols.extend(model_dict['model_features'])
-    output_cols.extend(model_dict['model_target_columns'])
-
-    output_cols.append('timestamp')
+    output_cols = copy(model_dict['analytic_logging_columns'])
 
     if CIVIS_SERVICE_VERSION:
         output_cols.append('civis_service_version')
@@ -130,28 +145,6 @@ def output_analytics_log_columns(model_dict):
 
     general_logger.info("Analytics logging columns: ")
     general_logger.info(','.join(output_cols))
-
-
-def _get_target_columns(metadata):
-    """Given ModelPipeline metadata, construct the list of columns output by
-    the model's prediction method.
-    """
-    target_cols = []
-    if metadata['model']['type'] == 'classification':
-        # Deal with multilabel models, if necessary
-        if len(metadata['data']['target_columns']) > 1:
-            for i, col in enumerate(metadata['data']['target_columns']):
-                for cls in metadata['data']['class_names'][i]:
-                    target_cols.append(col + '_' + str(cls))
-        else:
-            col = metadata['data']['target_columns'][0]
-            for cls in metadata['data']['class_names']:
-                target_cols.append(col + '_' + str(cls))
-    else:
-        for col in metadata['data']['target_columns']:
-            target_cols.append(col)
-
-    return target_cols
 
 
 def get_model_dict():
@@ -165,29 +158,63 @@ def get_model_dict():
         Dictionary containing objects and metadata for the desired CivisML
         modeling job.
     """
-    # Pull existing model
-    job_id = os.environ.get('MODEL_JOB_ID')
-    run_id = os.environ.get('MODEL_RUN_ID')
+    # Pull existing model and list of user and item columns
+    model_file_id = os.environ.get('MODEL_FILE_ID')
+    user_item_file_id = os.environ.get('USER_ITEM_COLUMNS_FILE_ID')
+    model, user_item_columns = _fetch_recommender(model_file_id,
+                                                  user_item_file_id)
 
-    model, model_pipe, model_features = _fetch_civisml_model(job_id, run_id)
+    # Make sparse diagonal item block; count users and items
+    n_users, items, diag_item_matrix = _get_users_items(user_item_columns)
 
     model_dict = {}
     model_dict['model'] = model
-    model_dict['model_pipe'] = model_pipe
-    model_dict['model_name'] = model_pipe.model_name
-    model_dict['model_features'] = model_features
+    model_dict['items'] = items
+    model_dict['n_users'] = n_users
+    model_dict['diag_item_matrix'] = diag_item_matrix
+    model_dict['user_item_columns'] = user_item_columns
+    model_dict['model_name'] = "Recommender file ID: {}".format(model_file_id)
 
-    metadata = model_pipe.train_result_.metadata
-
-    # Get relevant prediction method
-    if metadata['model']['type'] == 'classification':
-        model_dict['method'] = 'predict_proba'
-    else:
-        model_dict['method'] = 'predict'
-
-    # Get target columns
-    model_dict['model_target_columns'] = _get_target_columns(metadata)
+    model_dict['method'] = 'predict'
+    model_dict['model_target_columns'] = 'rating'
+    model_dict['analytic_logging_columns'] = ['user', 'item', 'rating',
+                                              'timestamp']
 
     general_logger.debug("model_dict: \n%s", model_dict)
 
     return model_dict
+
+
+def make_input_matrix(user, model_dict):
+    """Given a user and
+    """
+    user = str(user)
+
+    if not user.startswith(USER_PREFIX):
+        user = USER_PREFIX + user
+
+    columns = model_dict['user_item_columns']
+    n_users = model_dict['n_users']
+    n_items = len(model_dict['items'])
+
+    if user not in columns:
+        general_logger.info("User %s not found in in training columns", user)
+        user_block = sparse.csr_matrix((n_items, n_users))
+    else:
+        user_ind = np.argwhere(np.array(columns) == user).ravel()[0]
+        # first user
+        if user_ind == 0:
+            user_block = sparse.hstack((np.ones(shape=(n_items, 1)),
+                                        csr_matrix((n_items, n_users - 1))))
+        # last user
+        elif user_ind == n_users - 1:
+            user_block = sparse.hstack((csr_matrix((n_items, n_users - 1)),
+                                        np.ones(shape=(n_items, 1))))
+        # in between user
+        else:
+            user_block = sparse.hstack((csr_matrix((n_items, user_ind)),
+                                        np.ones(shape=(n_items, 1)),
+                                        csr_matrix((n_items,
+                                                    n_users - user_ind - 1))))
+
+    return sparse.hstack((user_block, model_dict['diag_item_matrix'])), user
